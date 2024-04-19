7e047f0816cf8812b8d7aba1ba2af9940d6f1951
AdAstraAspera <127353512+AdAstraAspera@users.noreply.github.com>
2024-04-19 02:41:24 -0300

    RSS feed testing 	modified:   app/api/chat/functions.ts

--- app/api/chat/functions.ts
+++ app/api/chat/functions.ts
@@ -1,10 +1,13 @@
+import Parser from 'rss-parser';
 import { CompletionCreateParams } from "openai/resources/chat/index";
+
+let parser = new Parser();
 
 export const functions: CompletionCreateParams.Function[] = [
   {
     name: "get_top_stories",
     description:
-      "Get the top stories from Hacker News. Also returns the Hacker News URL to each story.",
+      "Get the top stories from the Royal Gazette and Bernews RSS feeds. Also returns the URL to each story.",
     parameters: {
       type: "object",
       properties: {
@@ -15,105 +18,33 @@ export const functions: CompletionCreateParams.Function[] = [
       },
       required: [],
     },
+    action: async ({ limit = 10 }) => {
+      const feed1 = await parser.parseURL('https://navweb.royalgazette.com/feeds');
+      const feed2 = await parser.parseURL('http://feeds.feedburner.com/Bernewscom');
+      const stories = [...feed1.items, ...feed2.items].slice(0, limit);
+      return stories;
+    }
   },
   {
     name: "get_story",
     description:
-      "Get a story from Hacker News. Also returns the Hacker News URL to the story.",
+      "Get a story from the RSS feeds. Also returns the URL to the story.",
     parameters: {
       type: "object",
       properties: {
-        id: {
-          type: "number",
-          description: "The ID of the story",
+        guid: {
+          type: "string",
+          description: "The GUID of the story",
         },
       },
-      required: ["id"],
+      required: ["guid"],
     },
+    action: async ({ guid }) => {
+      const feed1 = await parser.parseURL('https://navweb.royalgazette.com/feeds');
+      const feed2 = await parser.parseURL('http://feeds.feedburner.com/Bernewscom');
+      const story = [...feed1.items, ...feed2.items].find(item => item.guid === guid);
+      return story;
+    }
   },
-  {
-    name: "get_story_with_comments",
-    description:
-      "Get a story from Hacker News with comments.  Also returns the Hacker News URL to the story and each comment.",
-    parameters: {
-      type: "object",
-      properties: {
-        id: {
-          type: "number",
-          description: "The ID of the story",
-        },
-      },
-      required: ["id"],
-    },
-  },
-  {
-    name: "summarize_top_story",
-    description:
-      "Summarize the top story from Hacker News, including both the story and its comments. Also returns the Hacker News URL to the story and each comment.",
-    parameters: {
-      type: "object",
-      properties: {},
-      required: [],
-    },
-  },
+  // ... rest of the functions
 ];
-
-async function get_top_stories(limit: number = 10) {
-  const response = await fetch(
-    "https://hacker-news.firebaseio.com/v0/topstories.json",
-  );
-  const ids = await response.json();
-  const stories = await Promise.all(
-    ids.slice(0, limit).map((id: number) => get_story(id)),
-  );
-  return stories;
-}
-
-async function get_story(id: number) {
-  const response = await fetch(
-    `https://hacker-news.firebaseio.com/v0/item/${id}.json`,
-  );
-  const data = await response.json();
-  return {
-    ...data,
-    hnUrl: `https://news.ycombinator.com/item?id=${id}`,
-  };
-}
-
-async function get_story_with_comments(id: number) {
-  const response = await fetch(
-    `https://hacker-news.firebaseio.com/v0/item/${id}.json`,
-  );
-  const data = await response.json();
-  const comments = await Promise.all(
-    data.kids.slice(0, 10).map((id: number) => get_story(id)),
-  );
-  return {
-    ...data,
-    hnUrl: `https://news.ycombinator.com/item?id=${id}`,
-    comments: comments.map((comment: any) => ({
-      ...comment,
-      hnUrl: `https://news.ycombinator.com/item?id=${comment.id}`,
-    })),
-  };
-}
-
-async function summarize_top_story() {
-  const topStory = await get_top_stories(1);
-  return await get_story_with_comments(topStory[0].id);
-}
-
-export async function runFunction(name: string, args: any) {
-  switch (name) {
-    case "get_top_stories":
-      return await get_top_stories();
-    case "get_story":
-      return await get_story(args["id"]);
-    case "get_story_with_comments":
-      return await get_story_with_comments(args["id"]);
-    case "summarize_top_story":
-      return await summarize_top_story();
-    default:
-      return null;
-  }
-}
